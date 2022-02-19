935ffc8491e58052ec72641ffec388575b144eb3
mikacha <onlymikacha@gmail.com>
2022-02-19 14:44:51 +0300

    I forgot about adding "main.py" into new commits...

--- main.py
+++ main.py
@@ -7,12 +7,26 @@ import click
 
 from colorama import init, Fore
 
-DEFAULT_PATH = '/home/mikacha/MyProgramms/tst/'
+import parse_codeforces
+
+
+DEFAULT_PATH = '/usr/bin/'
 DEFAULT_SOURCE_NAME = 'main'
+
+def set_default_filename(filename: str):
+    data = {}
+    with open(DEFAULT_PATH + 'config.json','r') as file:
+        data = json.load(file)
+
+    data['default_file_path'] = filename
+
+
+    with open(DEFAULT_PATH + 'config.json','w') as file:
+        data = json.dump(data, file)
 
 def get_sample_list() -> list[str]:
     result = []
-    with open('sample.txt') as file:
+    with open('samples.txt') as file:
         text = file.read()
         now = ''
         prev = ''
@@ -34,16 +48,16 @@ def execute_cpp():
                     process = subprocess.run([f'./{DEFAULT_SOURCE_NAME}'],
                     capture_output=True,
                     input=tests[i], text=True, timeout=1)
-        except subprocess.TimeoutExpired :
-            messages.append(Fore.RED + f'Test {i+1}: Time limit 1s!\nSample: ' + tests[i])
+        except subprocess.TimeoutExpired :            
+            messages.append(Fore.RED + f'Test {(i+2)//2}: Time limit 1s!\nSample: ' + tests[i])
             continue
         else:
             output = process.stdout.strip()
             if output!=tests[i+1].strip():
-                messages.append(Fore.RED + f'Test {i+1}: Wrong answer!\n' + Fore.RESET
-                        + f'Sample: {tests[i]}\nYour answer - {output}\nCorrect answer - {tests[i+1]}')
+                messages.append(Fore.RED + f'Test {(i+2)//2}: Wrong answer!\n' + Fore.RESET
+                        + f'Sample: {tests[i]}\nYour answer: \n {output}\nCorrect answer: \n {tests[i+1]}')
                 continue
-        messages.append(Fore.GREEN + f'Test {i+1}: OK!')
+        messages.append(Fore.GREEN + f'Test {(i+2)//2}: OK!')
     return messages
 
 def compile_cpp(filename: str) -> bool:
@@ -62,7 +76,7 @@ def compile_cpp(filename: str) -> bool:
         if os.path.exists(DEFAULT_SOURCE_NAME):
             os.system(f'rm {DEFAULT_SOURCE_NAME}')
 
-        subprocess.run([f'g++' , f'{DEFAULT_PATH+filename}', '-o', f'{DEFAULT_SOURCE_NAME}'], shell=False, capture_output=True)
+        subprocess.run([f'g++' , f'{filename}', '-o', f'{DEFAULT_SOURCE_NAME}'], shell=False, capture_output=True)
 
         if not os.path.exists(f'{DEFAULT_SOURCE_NAME}'):
             print(Fore.RED + 'Is seems you have a compilation error')
@@ -75,7 +89,7 @@ def get_default_filename() -> str:
         return json.load(file)['default_file_path']
 
 
-@click.command('run')
+@click.command('run', help='Start (compile) executable file and test it using samples at Samples.txt')
 @click.option('--filename', default=get_default_filename(), help='Set the default execute file')
 def run_source(filename: str):
     if not os.path.exists(filename):
@@ -91,30 +105,58 @@ def run_source(filename: str):
                     print(mes)
 
 
-@click.command('set-default')
+@click.command('set-default', help='Makes file executable')
 @click.argument('filename')
-def set_default_filename(filename: str):
-    data = {}
-    with open(DEFAULT_PATH + 'config.json','r') as file:
-        data = json.load(file)
+def set_default_filename_command(filename: str):
+    set_default_filename(filename)
 
-    data['default_file_path'] = filename
+@click.command('new', help='Makes a cpp file, which have tamplate code and makes it default executable file')
+@click.argument('filename')
+def makefile(filename: str):
+    text = ''
+    with open(DEFAULT_PATH+'base.cpp') as file:
+        text = file.read()
 
-    with open(DEFAULT_PATH + 'config.json','w') as file:
-        data = json.dump(data, file)
+    subprocess.run(['touch', f'{filename}'])
+    
+    with open(filename, 'w') as file:
+        file.write(text)
+    
+    set_default_filename(filename)
+
 
 @click.command('init', help='Makes dependency files')
 def init_files():
     os.system('touch prevfilename')
     os.system('touch samples.txt')
 
-@click.group(help="This programm allow you to automitize compilation and testing of your programms")
+@click.command('link', help='Parses samples from codeforces')
+@click.argument('url_link')
+def get_samples(url_link: str):
+    samples = parse_codeforces.parse_cf_task_tests(content=parse_codeforces\
+            .download_cf_task(url_link))
+
+    final_content = ''
+
+    for sample_test in samples:
+        final_content += sample_test.input_text + '\n'
+        final_content += sample_test.output_text + '\n'
+
+
+    with open('samples.txt', 'w') as file:
+        file.write(final_content)
+
+@click.group(help="This programm allows you to automitize compilation and testing\
+        of your programms")
 def main():
     pass
 
-main.add_command(set_default_filename)
+
+main.add_command(set_default_filename_command)
 main.add_command(run_source)
 main.add_command(init_files)
+main.add_command(makefile)
+main.add_command(get_samples)
 
 if __name__ == '__main__':
     main()
