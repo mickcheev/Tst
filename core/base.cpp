8159c3ae013aa33f2f787b16f30d32e964602e88
mikacha <onlymikacha@gmail.com>
2022-05-06 01:07:07 +0300

    updated base.cpp

--- core/base.cpp
+++ core/base.cpp
@@ -7,6 +7,7 @@
 #include <functional>
 #include <numeric>
 #include <string>
+#include <queue>
 
 using namespace std;
 
@@ -14,14 +15,28 @@ using namespace std;
 #define fori(i, n) for (int i = 0; i < (n); i++)
 #define fastIO() {ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);}
 
+template<class T> void print(T v) { for (auto x : v) cout << x << ' '; cout << endl; };
 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
 
-template<class T> void print(T v) { cout << sz(v) << endl; for (auto x : v) cout << x << ' '; cout << endl; };
 
 
 const int mod = 1000000000 + 7;
 const int INF = 1000000000 + 1;
 
+void solve() {
+    
+}
+
 int main() {
     fastIO();
+    
+    int t=1;
+
+    //cin >> t;
+
+    while (t--) 
+        solve();
 }
